179f25ce25e0b89df9250078947faebf846f6234
jacopo <jacopotrapani89@gmail.com>
2021-06-29 12:57:50 +0200

    enquiry fields

--- Messages/CreateEnquiry.php
+++ Messages/CreateEnquiry.php
@@ -4,5 +4,35 @@ namespace Myrooms\Messages\Messages;
 
 class CreateEnquiry
 {
+    private $unitId;
+    private $startDate;
+    private $endDate;
+    private $name;
+    private $surname;
+    private $email;
+    private $phoneNumber;
 
+    private const dateFormat = 'Y-m-d';
+
+    public function __construct(string $unitId, \DateTimeImmutable $startDate, \DateTimeImmutable $endDate, string $name, string $surname, string $phoneNumber, string $email)
+    {
+
+        \Assert\Assert::lazy()
+            ->that($name)->notNull()->string()
+            ->that($surname)->notNull()->string()
+            ->that($unitId)->notNull()->string()
+            ->that($startDate->format(self::dateFormat))->date( self::dateFormat)
+            ->that($endDate->format(self::dateFormat))->date(self::dateFormat)->greaterThan($checkIn->format(self::dateFormat))
+            ->that($email)->string()->email()
+            ->that($phoneNumber)->string()
+            ->verifyNow();
+
+        $this->name= $name;
+        $this->surname= $surname;
+        $this->startDate= $startDate;
+        $this->endDate= $endDate;
+        $this->email= $email;
+        $this->phoneNumber= $phoneNumber;
+        $this->unitId = $unitId;
+    }
 }
