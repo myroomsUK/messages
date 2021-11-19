7fd29f015361854bba7741c0d2105bb6c22b2274
gabriele <gabriele.alfano.1996@gmail.com>
2021-11-19 18:43:32 +0100

    AP Enquiry message

---
+++ Messages/Enquiry/EnquiryStatusChanged.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Myrooms\Messages\Messages\Enquiry;
+
+class EnquiryStatusChanged
+{
+    const ENQUIRY_ID = "enquiryId";
+    const STATUS = "status";
+
+    /**
+     * @var int
+     */
+    private $enquiryId;
+
+    /**
+     * @var EnquiryStatus
+     */
+    private $status;
+
+    public function __construct(int $enquiryId, EnquiryStatus $status)
+    {
+        \Assert\Assert::lazy()
+            ->that($enquiryId)->notNull()->integer()
+            ->that($status)->isInstanceOf(EnquiryStatus::class);
+
+        $this->enquiryId = $enquiryId;
+        $this->status = $status;
+    }
+
+    public function getEnquiryId(): int
+    {
+        return $this->enquiryId;
+    }
+
+    public function getStatus(): EnquiryStatus
+    {
+        return $this->status;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            self::ENQUIRY_ID => $this->enquiryId,
+            self::STATUS => $this->status->getStatus()
+        ];
+    }
+
+    public static function fromArray(array $array): self
+    {
+        return new self($array[self::ENQUIRY_ID], new EnquiryStatus($array[self::STATUS]));
+    }
+    
+}
